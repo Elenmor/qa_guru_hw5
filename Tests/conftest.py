a2f8ee5a584ead5f9f6b43cd7fea120fa96ce678
Elena Morilova <elena.morilova@mail.ru>
2023-04-19 17:04:33 +0400

    Успешная проверка

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from selene import browser
 
-@pytest.fixture()
-def browser_open(scope='function'):
+@pytest.fixture(scope='function')
+def browser_open():
     browser.driver.set_window_size(1920, 1080)
     browser.config.base_url = 'https://demoqa.com'
     yield browser
--- Tests/hw5_test.py
+++ Tests/hw5_test.py
@@ -1,12 +1,12 @@
-import os.path
+import os
 from selene.support.shared import browser
 from selene import have, command
-from selenium.webdriver.common.by import By
-from selenium import webdriver
+
 
 
 def test_practice_form(browser_open):
     browser.open('/automation-practice-form')
+
     # WHEN
 
     browser.element('#firstName').type('Helen')
@@ -15,7 +15,6 @@ def test_practice_form(browser_open):
 
     #Choose gender
     browser.element('.custom-control [for=gender-radio-2]').click()
-    #browser.element('#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(2) > label').click()
 
     browser.element('#userNumber').type('5555555555')
 
@@ -24,20 +23,13 @@ def test_practice_form(browser_open):
     browser.element('.react-datepicker__year-select').send_keys('1984')
     browser.element('.react-datepicker__month-select').send_keys('May')
     browser.element('.react-datepicker__day--012').click()
-    #browser.driver.find_element(By.CSS_SELECTOR, ".react-datepicker__year-select").click()
-    #browser.driver.find_element(By.XPATH, "//option[. = '1984']").click()
-    #browser.driver.find_element(By.CSS_SELECTOR, ".react-datepicker__month-select").click()
-    #browser.driver.find_element(By.XPATH, "//option[. = 'May']").click()
-    #browser.driver.find_element(By.CSS_SELECTOR, ".react-datepicker__day--012").click()
 
     #Choose subject and hobby
     browser.element('#subjectsInput').type('Art').press_enter()
     browser.element('.custom-control [for=hobbies-checkbox-1]').click()
-    #browser.element('#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label').click()
 
     #Upload piture
-    browser.element('#uploadPicture').send_keys(os.path.abspath('C:/Users/Денис/PycharmProjects/qa_guru_hw5/Tests/resources/pic.png'))
-    #browser.element('#uploadPicture').send_keys(os.getcwd() + "/pic.png")
+    browser.element('#uploadPicture').send_keys(os.getcwd() + "/pic.png")
 
     #Choose adress
     browser.element('#currentAddress').type('adress street, 1')
